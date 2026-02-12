a028ce1cef4117527455611ab3217336de00f700
myagmarulzii <33541806+myagmarulzii@users.noreply.github.com>
2026-02-12 16:37:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,9 @@ scope = [
     "https://www.googleapis.com/auth/drive",
 ]
 
- creds = ServiceAccountCredentials.from_json_keyfile_dict(
+creds = ServiceAccountCredentials.from_json_keyfile_dict(
     st.secrets["gcp_service_account"],
     scope
-
 )
 
 client = gspread.authorize(creds)
